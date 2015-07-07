b11a50ef5e8b571a7822bd172aff4866e160a955
david garber & bob ellis <student@epicodus.com>
2015-07-07 10:04:16 -0700

    Add crud functionality to questions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,25 @@ class QuestionsController < ApplicationController
     end
 end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
   private
   def question_params
     params.require(:question).permit(:text)
