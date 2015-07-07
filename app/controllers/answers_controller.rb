c8b337d115217b4d88c8b0ed3a766d11e7f4d91e
david garber & bob ellis <student@epicodus.com>
2015-07-07 11:38:44 -0700

    fixed bug with CRUD

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
   def new
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.new
+    @answer = Answer.new
   end
 
   def create
@@ -12,7 +12,29 @@ class AnswersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+  if @answer.update(answer_params)
+    redirect_to question_path(@answer.question)
+  else
+    render :edit
+  end
 end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+      redirect_to question_path(@answer.question)
+  end
 
 private
   def answer_params
